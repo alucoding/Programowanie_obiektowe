6206240a8f9c44d714bfbc2623c5d7fda54bd3dd
alucoding <albertkruk20@gmail.com>
2022-06-17 13:20:11 +0200

    LAB_01

---
+++ LAB_01/Car.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_01
+{
+    public class Car
+    {
+        private string _brand;
+        private string _model;
+        private int _doorCount;
+        private float _engineVolume;
+        private double _avgConsump;
+        private string _RegistrationNumber;
+        private static int _carCount = 0;
+        private double consump;
+        private double cost;
+
+        public string Brand { get { return _brand; } set { _brand = value; } }
+        public string Model { get { return _model; } set { _model = value; } }
+        public int DoorCount { get { return _doorCount; } set { _doorCount = value;} }
+        public float EngineVolume { get { return _engineVolume; } set { _engineVolume = value;} }
+        public double AvgConsump { get { return _avgConsump; } set { _avgConsump = value; } }
+        public string RegistrationNumber { get { return _RegistrationNumber; } set { _RegistrationNumber = value; } }
+
+        public Car()
+        {
+            _brand = "nieznany";
+            _model = "nieznany";
+            _doorCount = 0;
+            _engineVolume = 0;
+            AvgConsump = 0.0;
+            _carCount++;
+        }
+
+        public Car(string brand, string model, int doorCount, float engineVolume, double avgConsump, string RegistrationNumber)
+        {
+           _brand =brand;
+            _model = model;
+            _doorCount = doorCount;
+            _engineVolume = engineVolume;
+            _avgConsump = avgConsump;
+            _RegistrationNumber = RegistrationNumber;
+            _carCount++;
+        }
+        public double CalculateConsump(double roadLength)
+        {
+            return consump = (AvgConsump * roadLength) / 100;
+        }
+
+        public double CalculateCost(double roadLength, double petrolCost)
+        {
+            return cost = consump * petrolCost;
+        }
+
+
+        public override string ToString()
+        {
+            return $" Car | Brand: {_brand}, Model: {_model}, DoorCount: {_doorCount}, EngineVolume: {_engineVolume}, AvgConsump: {_avgConsump}, RegistrationNumber: {_RegistrationNumber}";
+        }
+
+        public void Details()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        public static void DisplayCarCount()
+        {
+            Console.WriteLine(_carCount);
+        }
+    }
+}
---
+++ LAB_01/Garage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_01
+{
+    public class Garage
+    {
+        private string _address;
+        private int _carsCount;
+        private int _capacity;
+        private Car[] _cars;
+
+        public string Address { get { return _address; } set { _address = value; } }
+        public int Capacity { get { return _capacity; } set { _capacity = value; _cars = new Car[value]; } }
+
+
+        public Garage()
+        {
+            _address = "nieznany";
+            _carsCount = 0;
+            _capacity = 0;
+            _cars = null;
+        }
+
+        public Garage(string address, int capacity)
+        {
+            _address = address;
+            _carsCount = capacity;
+        }
+    }
+}
